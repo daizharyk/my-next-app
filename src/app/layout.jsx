0ff9519f35b20b724cf7dcdac6f3dd1bac91233d
Didar <aizharyk78@gmail.com>
2025-04-10 23:30:36 +0500

    1

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 // app/layout.js
 import Header from "./components/Header";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footer/Footer";
 import "./global.css";
 
 export default function Layout({ children }) {
